4874235a9ab9201af7d7399c494d6b0025bf1863
nandakoryaaa <dr_noise@mail.ru>
2020-11-26 16:54:08 +0300

    main fix.

---
+++ src/minesweeper.c
@@ -0,0 +1,45 @@
+#define SDL_MAIN_HANDLED
+
+#include <SDL2/SDL.h>
+#include <stdio.h>
+
+unsigned char field[100 * 100];
+
+int main(int argc, char* argv[]) {
+
+    SDL_Window *window;
+    SDL_Event event;
+
+    SDL_Init(SDL_INIT_VIDEO);
+
+    window = SDL_CreateWindow(
+        "An SDL2 window",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        640,
+        480,
+        SDL_WINDOW_OPENGL
+    );
+
+    if (window == NULL) {
+        printf("Could not create window: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Surface *screenSurface = SDL_GetWindowSurface(window);
+    SDL_Rect rect = {100, 100, 200, 200};
+    SDL_FillRect(screenSurface, &rect, SDL_MapRGB(screenSurface->format, 255, 0, 0));
+    SDL_UpdateWindowSurface(window);
+
+    while (1) {
+        SDL_WaitEvent(&event);
+        if (event.type == SDL_QUIT) {
+            break;
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
